d3be7eb981e0508301596c559a67e0ab93752184
MitchCarroll <c.mitch.carroll@gmail.com>
2014-01-25 00:14:31 -0600

    Runs. All it does is display the app icon on the screen. whoopee.

--- GameTestBed/src/main/java/com/MitchCarroll/gametestbed/GameView.java
+++ GameTestBed/src/main/java/com/MitchCarroll/gametestbed/GameView.java
@@ -2,16 +2,44 @@ package com.MitchCarroll.gametestbed;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 /**
  * Created by mowgli on 1/20/14.
  */
-public class GameView extends SurfaceView {
-    private Bitmap b = Bitmap.createBitmap(100,100, Bitmap.Config.ARGB_8888);
-    private Canvas c = new Canvas(b);
+public class GameView extends SurfaceView implements SurfaceHolder.Callback {
+    private Bitmap bmp;
+    private SurfaceHolder holder;
 
     public GameView(Context context) {
         super(context);
+        holder = getHolder();
+        holder.addCallback(this);
+        bmp = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher);
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        Canvas c = holder.lockCanvas(null);
+        onDraw(c);
+        holder.unlockCanvasAndPost(c);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawColor(Color.BLACK);
+        canvas.drawBitmap(bmp, 10, 10, null);
     }
 }
--- GameTestBed/src/main/java/com/MitchCarroll/gametestbed/MainActivity.java
+++ GameTestBed/src/main/java/com/MitchCarroll/gametestbed/MainActivity.java
@@ -1,23 +1,15 @@
 package com.MitchCarroll.gametestbed;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
 
 public class MainActivity extends Activity {
+    GameView gv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        gv = new GameView(this);
+        setContentView(gv);
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
 }
